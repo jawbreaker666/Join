e7e52c2c3c416d1c93311a2fc57e7ff7aabcf8fe
jawbreaker666 <andrasev96@web.de>
2021-12-14 10:39:59 +0100

    first commit

---
+++ addtask.js
@@ -0,0 +1,36 @@
+let allTasks = [];
+
+function addTask(){
+    let titel = document.getElementById('titel').value;
+    let category = document.getElementById('category').value;
+    let description = document.getElementById('description').value;
+    let date = document.getElementById('date').value;
+    let urgency = document.getElementById('urgency').value;
+    let assigned = 'Laura Andrasev';
+    let task = {
+        'titel':  titel,
+        'category': category,
+        'description': description,
+        'date': date,
+        'urgency': urgency,
+        'assigned': assigned,
+        
+
+    };
+
+   
+
+    allTasks.push(task);
+    
+    let allTasksAsString = JSON.stringify(allTasks);
+   localStorage.setItem('allTasks', allTasksAsString);
+
+
+}
+
+function loadTasks(){
+    let allTasksAsString = JSON.parse(allTasksAsString);
+    
+    allTasks.push
+    console.log('Loaded allTasks', allTasks);
+}
